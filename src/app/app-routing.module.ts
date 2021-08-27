e99028f0e76baf9add71bfa31ab2675a683ba8b0
Anibal <anibal78e@gmail.com>
2021-08-27 17:55:55 -0400

    visitor login, home, register screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'visitor/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'visitor',
+    loadChildren: () => import('./visitor/visitor.module')
+    .then(m => m.VisitorModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/visitor/visitor.module.ts
+++ src/app/visitor/visitor.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VisitorRoutingModule } from './visitor-routing.module';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LoginScreenComponent } from './views/login-screen/login-screen.component';
 import { RegisterScreenComponent } from './views/register-screen/register-screen.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeScreenComponent } from './views/home-screen/home-screen.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     LoginScreenComponent,
-    RegisterScreenComponent
+    RegisterScreenComponent,
+    HomeScreenComponent
   ],
   imports: [
     CommonModule,
