525d623c1dc7b3a1b52c9436f64aae0d0f0c84ec
Anibal <anibal78e@gmail.com>
2021-09-03 15:17:41 -0400

    user changes

--- src/app/visitor/visitor.module.ts
+++ src/app/visitor/visitor.module.ts
@@ -5,7 +5,11 @@ import { LoginScreenComponent } from './views/login-screen/login-screen.componen
 import { RegisterScreenComponent } from './views/register-screen/register-screen.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeScreenComponent } from './views/home-screen/home-screen.component';
-
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CdkStepperModule} from '@angular/cdk/stepper';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,12 @@ import { HomeScreenComponent } from './views/home-screen/home-screen.component';
   imports: [
     CommonModule,
     VisitorRoutingModule,
-
+    MatStepperModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CdkStepperModule
   ]
 })
 export class VisitorModule { }
