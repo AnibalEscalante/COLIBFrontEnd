a2b4f8b5356e8687e336df5752ff47e247e24da7
Anibal <anibal78e@gmail.com>
2021-09-03 18:10:27 -0400

    register info

--- src/app/visitor/views/register-screen/register-screen.component.ts
+++ src/app/visitor/views/register-screen/register-screen.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {CdkStepper} from '@angular/cdk/stepper';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 export class CdkCustomStepperWithoutFormExample {}
 @Component({
@@ -9,6 +8,7 @@ export class CdkCustomStepperWithoutFormExample {}
   styleUrls: ['./register-screen.component.less']
 })
 export class RegisterScreenComponent implements OnInit {
+  hide = true;
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
 
@@ -21,5 +21,14 @@ export class RegisterScreenComponent implements OnInit {
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ''
     });
+  }
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 }
