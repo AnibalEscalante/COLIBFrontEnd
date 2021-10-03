b9add98f2f7df6a7c89ed605864f70e81dd7fadf
Anibal <anibal78e@gmail.com>
2021-10-03 16:32:23 -0300

    visitor info

--- src/app/visitor/components/project/project.component.ts
+++ src/app/visitor/components/project/project.component.ts
@@ -8,12 +8,23 @@ import { Project } from '../../../core/models/project.model';
 })
 export class ProjectComponent implements OnInit {
   
-  @Input() project!: Project;
+  @Input() 
+  project!: Project;
+
   @Output() readMoreEvent = new EventEmitter<any>();
+
+  @Output() show= new EventEmitter<boolean>();
+  
+  public showinfo: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  showComponent(){
+    this.showinfo = true;
+    this.show.emit(this.showinfo)
   }
 
   sendProjectInfo() {
