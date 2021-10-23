87039d16f0ef4a6d0aff1a1e4413c1f0a9b1a398
Anibal <anibal78e@gmail.com>
2021-10-23 17:09:44 -0300

    login funcional

--- src/app/visitor/views/login-screen/login-screen.component.ts
+++ src/app/visitor/views/login-screen/login-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-login-screen',
@@ -8,20 +10,53 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class LoginScreenComponent implements OnInit {
   
-  email = new FormControl('', [Validators.required, Validators.email]);
+  emailreq = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
+  registerForm: FormGroup;
 
-  constructor() { }
   
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.emailreq.hasError('required')) {
       return 'Correo electrónico no valido';
     }
 
-    return this.email.hasError('email') ? 'Ese no es un correo electrónico' : '';
+    return this.emailreq.hasError('email') ? 'Ese no es un correo electrónico' : '';
   }
 
   ngOnInit(): void {
   }
+
+  constructor( 
+
+    private authService: AuthService,
+    private router: Router, 
+    private formBuilder: FormBuilder,
+
+) {
+
+  this.registerForm = this.formBuilder.group({
+
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]}
+
+  );
+}
+
+
+get email() { return this.registerForm?.get('email'); }
+get password() { return this.registerForm?.get('password'); }
+
+
+
+async onSubmit() {
+  const email = this.email?.value;
+  const password = this.password?.value;
+  try {
+    await this.authService.login(email!,password!).toPromise();
+  } catch (error) {
+    console.log('xd')
+  }
+
+}
 
 }
