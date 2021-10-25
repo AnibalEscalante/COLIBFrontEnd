820cb7d536b6fd48d4f16431b4c9f05ebc9e39f0
Anibal <anibal78e@gmail.com>
2021-10-25 15:51:24 -0300

    login

--- src/app/visitor/views/login-screen/login-screen.component.ts
+++ src/app/visitor/views/login-screen/login-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-login-screen',
@@ -8,20 +10,49 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class LoginScreenComponent implements OnInit {
   
-  email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
+  public emailreq = new FormControl('', [Validators.required, Validators.email]);
+  public hide = true;
+  
+  public registerForm: FormGroup;
+  public messageFlag = 0;
+  constructor(
+    private authService: AuthService,
+    private router: Router, 
+    private formBuilder: FormBuilder,
+  ) { 
+    this.registerForm = this.formBuilder.group({
 
-  constructor() { }
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]}
+
+    );
+  }
   
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.emailreq.hasError('required')) {
       return 'Correo electrónico no valido';
     }
 
-    return this.email.hasError('email') ? 'Ese no es un correo electrónico' : '';
+    return this.emailreq.hasError('email') ? 'Ese no es un correo electrónico' : '';
   }
 
   ngOnInit(): void {
+  }
+
+  get email() { return this.registerForm?.get('email'); }
+  get password() { return this.registerForm?.get('password'); }
+
+
+  async onSubmit() {
+    const email = this.email?.value;
+    const password = this.password?.value;
+    try {
+      await this.authService.login(email!,password!).toPromise();
+      this.messageFlag = 0;
+    } catch (error) {
+      this.messageFlag = 1;
+    }
+
   }
 
 }
