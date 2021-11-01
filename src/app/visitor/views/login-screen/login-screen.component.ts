b725c7beb27d48b851755e7c65982f127882ecde
NikolayCarrasco <nikolay.carrasco.v@gmail.com>
2021-11-01 17:41:16 -0300

    validators login

--- src/app/visitor/views/login-screen/login-screen.component.ts
+++ src/app/visitor/views/login-screen/login-screen.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-screen',
@@ -9,50 +10,59 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
   styleUrls: ['./login-screen.component.less']
 })
 export class LoginScreenComponent implements OnInit {
-  
-  public emailreq = new FormControl('', [Validators.required, Validators.email]);
   public hide = true;
   
   public registerForm: FormGroup;
-  public messageFlag = 0;
+  public message!: string;
   constructor(
     private authService: AuthService,
-    private router: Router, 
     private formBuilder: FormBuilder,
+    private toastr: ToastrService
   ) { 
-    this.registerForm = this.formBuilder.group({
-
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]}
-
+      this.registerForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9@$!%*?&.]{8,12}$')]]
+      }
     );
-  }
-  
-  getErrorMessage() {
-    if (this.emailreq.hasError('required')) {
-      return 'Correo electrónico no valido';
-    }
-
-    return this.emailreq.hasError('email') ? 'Ese no es un correo electrónico' : '';
   }
 
   ngOnInit(): void {
+  }
+  
+  getEmailErrorMessage() {
+    if (this.email?.hasError('required')) {
+      return 'Ingrese un correo electrónico';
+    }
+    return this.email?.invalid ? 'El dato ingresado no es un correo electónico valido' : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.password?.hasError('required')) {
+      return 'Ingrese la contraseña';
+    }
+    return this.password?.invalid ? 'El dato ingresado no es una contraseña valida' : '';
   }
 
   get email() { return this.registerForm?.get('email'); }
   get password() { return this.registerForm?.get('password'); }
 
-
   async onSubmit() {
     const email = this.email?.value;
     const password = this.password?.value;
     try {
-      await this.authService.login(email!,password!).toPromise();
-      this.messageFlag = 0;
+      /* if (this.email?.valid && this.password?.valid) {
+        await this.authService.login(email!, password!).toPromise();
+        this.toastr.success('Inicio de sesión exitoso');
+      }
+      else {
+        this.toastr.error('Datos del inicio de sesión invalidos');
+      } */
+      await this.authService.login(email!, password!).toPromise();
+      this.toastr.success('Inicio de sesión exitoso');
     } catch (error) {
-      this.messageFlag = 1;
+      this.toastr.error('Datos del inicio de sesión invalidos');
     }
-
+    
   }
 
 }
